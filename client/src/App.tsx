8f2312ca342abd5f7d1dd6811919181d061d28eb
Maxime TRICOCHE <maxime.trche@gmail.com>
2025-01-09 15:18:56 +0100

    [FEAT] : add layout and footer component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,44 @@
+import { Link, Outlet } from "react-router-dom";
 import "./App.css";
+import Footer from "./components/Footer";
 
 function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <div className="bg-text-secondary flex min-h-screen">
+      <header className="w-full h-20 bg-text-secondary lg:bg-bg_opacity-secondary flex lg:flex-col lg:w-1/5 lg:h-screen z-10">
+        <section className="flex lg:justify-center px-4 py-6 w-full ">
+          <Link to={"/"} className="flex items-center gap-4">
+            <img
+              src="./src/assets/images/logo.png"
+              alt="blablabla"
+              aria-labelledby="wildcom"
+              className="w-12 lg:w-12 xl:w-16  2xl:w-20"
+            />
+
+            <h1
+              className="hidden font-title font-semibold lg:block lg:text-accent-primary lg:text-1xl xl:text-2xl  2xl:text-3xl"
+              id="wildcom"
+            >
+              &lt;WildCom/&gt;
+            </h1>
+          </Link>
+        </section>
+      </header>
+
+      <main className="flex flex-grow ">
+        <Outlet />
+
+        <div className="w-52 h-52 rounded-full fixed top-1 -left-20 bg-accent-primary blur-[150px] opacity-65 lg:w-96 lg:h-96 lg:top-20 lg:left-80" />
+
+        <div className="w-52 h-52 rounded-full fixed bottom-16 -right-20 bg-accent-primary blur-[150px] opacity-65 lg:w-96 lg:h-96 lg:right-60" />
+      </main>
+
+      <footer className="w-1/5  bg-bg_opacity-secondary lg:flex flex-col hidden">
+        <section className="flex flex-grow" />
+        <Footer />
+      </footer>
+    </div>
+  );
 }
 
 export default App;
