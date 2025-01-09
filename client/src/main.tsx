25a807c58b8f6b9509e1364d74d10824a2126cdf
Maxime TRICOCHE <maxime.trche@gmail.com>
2025-01-10 00:38:12 +0100

    [FEAT] : add event page and route

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -8,6 +8,7 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom";
 // Import the main app component
 import App from "./App";
 import Home from "./pages/Home";
+import Events from "./pages/Events";
 
 // Import additional components for new routes
 // Try creating these components in the "pages" folder
@@ -27,6 +28,10 @@ const router = createBrowserRouter([
       {
         index: true,
         element: <Home />,
+      },
+      {
+        path: "/events",
+        element: <Events />,
       },
     ],
   },
