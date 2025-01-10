14827bc6ac5de591037196c9cdc24d6246747d0a
Alexhian <alexandrehiance@gmail.com>
2025-01-10 11:08:47 +0100

    [FEAT]: Admin dashboard done

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -7,10 +7,10 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
 // Import the main app component
 import App from "./App";
+import Admin from "./pages/Admin";
 import Events from "./pages/Events";
 import Home from "./pages/Home";
 import Profil from "./pages/Profil";
-import Admin from "./pages/Admin";
 
 // Import additional components for new routes
 // Try creating these components in the "pages" folder
