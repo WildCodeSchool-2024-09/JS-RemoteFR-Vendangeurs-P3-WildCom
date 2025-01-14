be0f22682649aab33dd0fdb0f5a17a25adca9231
jerem2802 <creamedoc@gmail.com>
2025-01-14 19:17:24 +0100

    [FEAT] add profil description

--- client/src/pages/Profil.tsx
+++ client/src/pages/Profil.tsx
@@ -1,5 +1,24 @@
 function Profil() {
-  return <div className="text-text-primary">profile</div>;
+  return (
+    <section className="relative z-10 flex gap-16 mx-0 bg-bg_opacity-primary border-bg_opacity-secondary font-text text-text-primary pb-11">
+      <div className="gap-4 pt-4 mx-4">
+        <img
+          className="w-48 mb-6 rounded-full "
+          src="src/assets/images/pictureprofil.webp"
+          alt="profile user"
+        />
+        <p>Biographie :</p>
+      </div>
+      <div className="flex flex-col gap-8 pt-3">
+        <p className="text-2xl font-bold">Sam Diswar</p>
+        <ul className="space-y-4">
+          <li>Github : </li>
+          <li>Linkedin : </li>
+          <li>Sites : </li>
+        </ul>
+      </div>
+    </section>
+  );
 }
 
 export default Profil;
