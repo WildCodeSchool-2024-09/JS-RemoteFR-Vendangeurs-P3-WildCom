b86c03feda5e7a22e6b05211608580dfd9105875
Maxime TRICOCHE <maxime.trche@gmail.com>
2025-01-17 09:56:48 +0100

    [FIX]: resolve conflit merge

--- server/database/schema.sql
+++ server/database/schema.sql
@@ -5,6 +5,10 @@ create table user (
   lastname varchar(255) not null,
   password varchar(255) not null,
   avatar varchar(255) null,
+  github VARCHAR(255) NULL,
+  linkedin VARCHAR(255) NULL,
+  site VARCHAR(255) NULL,
+  biography text NULL,
   is_admin boolean not null default false
 );
 
