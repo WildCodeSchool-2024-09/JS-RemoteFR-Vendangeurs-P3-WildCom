ffbf02158dbc79feebea8b40ab8fe63213c938a5
Maxime TRICOCHE <maxime.trche@gmail.com>
2025-01-29 16:48:30 +0100

    [FEAT]: add modification system for comments on events

--- client/src/components/CardEvent.tsx
+++ client/src/components/CardEvent.tsx
@@ -198,13 +198,14 @@ export const CardEvent: React.FC<CardEventProps> = ({ events }) => {
             }`}
           >
             {commentsVisibility[event.id] && (
-              <CommentEvent eventId={event.id} />
+              <>
+                <CommentEvent eventId={event.id} />
+
+                <section>
+                  <CommentInputEvent eventId={event.id} />
+                </section>
+              </>
             )}
-            <section>
-              {commentsVisibility[event.id] && (
-                <CommentInputEvent eventId={event.id} />
-              )}
-            </section>
           </footer>
         </article>
       ))}
